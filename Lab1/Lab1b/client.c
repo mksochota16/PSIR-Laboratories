4b2cf1d39c4e368c437d3cae33dfac94280919e0
shaggy17goo <michalwawrzynczak00@gmail.com>
2022-03-18 19:03:12 -0400

    Lab1b code reorganization, extract params for the exercise requirements

--- Lab1/Lab1b/client.c
+++ Lab1/Lab1b/client.c
@@ -1,22 +1,21 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netdb.h>
-#include <errno.h>
 #include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-
+#include <string.h>
+#include <sys/socket.h>
 
 #define SERVER "127.0.0.1"
+#define PORT "17170"
 #define MAX_BUF 128
 
 int main() {
     struct addrinfo h, *r, c;
     int s;
     unsigned int c_len = sizeof(c);
+
     char to_send[MAX_BUF];
     char received[MAX_BUF];
 
@@ -24,39 +23,40 @@ int main() {
     h.ai_family = PF_INET;
     h.ai_socktype = SOCK_DGRAM;
 
-    if (getaddrinfo(SERVER, "12345", &h, &r) != 0) {
-        printf("addrinfo error\n");
+    if (getaddrinfo(SERVER, PORT, &h, &r) != 0) {
+        printf("ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__);
+        exit(1);
     }
 
     if ((s = socket(r->ai_family, r->ai_socktype, r->ai_protocol)) == -1) {
-        printf("socket error\n");
+        printf("ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__);
+        exit(1);
     }
 
     snprintf(to_send, MAX_BUF, "hello_message");
-
     size_t pos = sendto(s, to_send, strlen(to_send), 0, r->ai_addr, r->ai_addrlen);
     if (pos < 0) {
-        printf("ERROR: %s\n", strerror(errno));
-        exit(-4);
+        printf("ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__);
+        exit(1);
     }
 
-    for(;;){
-        pos=recvfrom(s, received, MAX_BUF, 0, (struct sockaddr*)&c, &c_len );
+
+    for (;;) {
+        pos = recvfrom(s, received, MAX_BUF, 0, (struct sockaddr *) &c, &c_len);
         if (pos < 0) {
-            printf("ERROR: %s\n", strerror(errno));
-            exit(-4);
+            printf("ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__);
+            exit(1);
         }
 
         received[pos] = '\0';
         printf("Recv(%s:%d): %s\n", inet_ntoa(((struct sockaddr_in *) &c)->sin_addr),
                ntohs(((struct sockaddr_in *) &c)->sin_port), received);
 
-        snprintf(to_send, MAX_BUF, "RE: %s", received);
+        snprintf(to_send, MAX_BUF, "RE:%s", received);
         pos = sendto(s, to_send, strlen(to_send), 0, r->ai_addr, r->ai_addrlen);
         if (pos < 0) {
-            printf("ERROR: %s\n", strerror(errno));
-            exit(-4);
+            printf("ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__);
+            exit(1);
         }
-
     }
 }
--- Lab1/Lab1b/server.c
+++ Lab1/Lab1b/server.c
@@ -1,17 +1,29 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netdb.h>
-#include <errno.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
 
+#define PORT "17170"
 #define MAX_BUF 128
 #define MAX_CLIENT_COUNT 10
+#define SEND_INTERVAL_FLOOR 100000
+#define SEND_INTERVAL_CEILING 1570000
+#define SERVER_MESSAGE_LEN 18
+
+void gen_random_str(char *s, int len);
+
+void get_time(char *buff);
+
+void *listener(void *param);
+
+void *sender(void *param);
 
 struct conninfo {
     struct addrinfo *c;
@@ -22,10 +34,51 @@ struct conninfo {
     struct sockaddr_in *clients;
 };
 
-void gen_random_str(char *s, int len);
-void get_time(char *buff);
+//Przydalo by sie zrobic jakis "protokol" warstwy aplikacyjnej i [naglowek][message]
+//i w naglowku info czy to wiadomosc HELLO, czy wiadomosc z danymi(odpowiedz na dane z servera)
 
-void *listen_for_new_clients(void *param) {
+int main() {
+    struct addrinfo hints, *res, c;
+    unsigned int c_len = sizeof(c);
+    int s;
+
+    struct sockaddr_in clients[MAX_CLIENT_COUNT];
+    int clients_cnt = 0;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = PF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if (getaddrinfo(NULL, PORT, &hints, &res) != 0) {
+        printf("ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__);
+        exit(1);
+    }
+
+    if ((s = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1) {
+        printf("ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__);
+        exit(1);
+    }
+
+    if (bind(s, res->ai_addr, res->ai_addrlen) != 0) {
+        printf("ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__);
+        exit(1);
+    }
+
+    struct conninfo info = {
+            &c, &s, &c_len, &clients_cnt, clients
+    };
+
+    pthread_t listener_id;
+    pthread_t sender_id;
+
+    pthread_create(&listener_id, NULL, listener, (void *) &info);
+    pthread_create(&sender_id, NULL, sender, (void *) &info);
+
+    pthread_exit(NULL);
+}
+
+void *listener(void *param) {
     char received[MAX_BUF];
     char time_str[MAX_BUF];
     char hello_massage[] = "hello_message";
@@ -38,27 +91,27 @@ void *listen_for_new_clients(void *param) {
     for (;;) {
         ssize_t pos = recvfrom(local_s, received, MAX_BUF, 0, (struct sockaddr *) &info->c, info->c_len);
         if (pos < 0) {
-            printf("ERROR: %s\n", strerror(errno));
-            exit(-4);
+            printf("ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__);
+            exit(1);
         }
 
         received[pos] = '\0';
         if (memcmp(received, hello_massage, sizeof(hello_massage)) == 0) {
-            memcpy(&(info->clients[(*info->clients_cnt) % MAX_CLIENT_COUNT]), (struct sockaddr_in *) &(info->c), sizeof(struct addrinfo));
+            memcpy(&(info->clients[(*info->clients_cnt) % MAX_CLIENT_COUNT]), (struct sockaddr_in *) &(info->c),
+                   sizeof(struct addrinfo));
             (*info->clients_cnt)++;
             printf("New client: (%s:%d)\n", inet_ntoa(((struct sockaddr_in *) &(info->c))->sin_addr),
                    ntohs(((struct sockaddr_in *) &info->c)->sin_port));
-        }
-        else{
-
+        } else {
             get_time(time_str);
-            printf("[TIME]:%s, [SRC]:(%s:%d), [MSG]: ''%s''\n", time_str, inet_ntoa(((struct sockaddr_in *) &(info->c))->sin_addr),
+            printf("[TIME]:%s | [SRC]:(%s:%d) | [MSG]: %s\n", time_str,
+                   inet_ntoa(((struct sockaddr_in *) &(info->c))->sin_addr),
                    ntohs(((struct sockaddr_in *) &info->c)->sin_port), received);
         }
     }
 }
 
-void *send_to_random_client(void *param) {
+void *sender(void *param) {
     char to_send[MAX_BUF];
     struct conninfo *info = (struct conninfo *) param;
     srand(time(NULL));
@@ -67,84 +120,43 @@ void *send_to_random_client(void *param) {
     int local_s = *info->s;
 
     for (;;) {
-        usleep(500000);
-        if(*info->clients_cnt == 0)
+        int time_to_sleep = (rand() % (SEND_INTERVAL_CEILING - SEND_INTERVAL_FLOOR)) + SEND_INTERVAL_FLOOR;
+        usleep(time_to_sleep);
+        if (*info->clients_cnt == 0)
             continue;
 
         int random_client = rand() % *info->clients_cnt;
         struct sockaddr *picked_client = (struct sockaddr *) &(info->clients[random_client]);
-        gen_random_str(to_send,20);
+        gen_random_str(to_send, SERVER_MESSAGE_LEN);
 
-        size_t pos = sendto(local_s, to_send, strlen(to_send), 0,picked_client, *info->c_len);
+        size_t pos = sendto(local_s, to_send, strlen(to_send), 0, picked_client, *info->c_len);
         if (pos < 0) {
-            printf("ERROR: %s\n", strerror(errno));
-            exit(-4);
+            printf("ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__);
+            exit(1);
         }
     }
 }
 
 
-int main() {
-    struct addrinfo h, *r, c;
-    int s;
-    unsigned int c_len = sizeof(c);
-
-    struct sockaddr_in clients[MAX_CLIENT_COUNT];
-    int clients_cnt = 0;
-
-    memset(&h, 0, sizeof(struct addrinfo));
-    h.ai_family = PF_INET;
-    h.ai_socktype = SOCK_DGRAM;
-    h.ai_flags = AI_PASSIVE;
-
-    if (getaddrinfo(NULL, "12345", &h, &r) != 0) {
-        printf("addrinfo error\n");
-    }
-
-    if ((s = socket(r->ai_family, r->ai_socktype, r->ai_protocol)) == -1) {
-        printf("socket error\n");
-    }
-
-
-    if (bind(s, r->ai_addr, r->ai_addrlen) != 0) {
-        printf("bind error\n");
-    }
-
-    pthread_t listen_for_new_clients_id;
-    pthread_t send_to_random_client_id;
-
-    struct conninfo info = {
-            &c, &s, &c_len, &clients_cnt, clients
-    };
-
-    pthread_create(&listen_for_new_clients_id, NULL, listen_for_new_clients, (void *) &info);
-    pthread_create(&send_to_random_client_id, NULL, send_to_random_client, (void *) &info);
-
-    pthread_exit(NULL);
-}
-
 void gen_random_str(char *s, int len) {
-    static const char alphanum[] =     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    static const char alphanum[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     for (int i = 0; i < len; ++i) {
         s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
     }
-
     s[len] = 0;
 }
 
-void get_time(char *buff){
+void get_time(char *buff) {
     time_t now;
     time(&now);
     struct tm *local = localtime(&now);
     int hours = local->tm_hour;         // get hours since midnight (0-23)
     int minutes = local->tm_min;        // get minutes passed after the hour (0-59)
     int seconds = local->tm_sec;        // get seconds passed after a minute (0-59)
-
     int day = local->tm_mday;            // get day of month (1 to 31)
     int month = local->tm_mon + 1;      // get month of year (0 to 11)
     int year = local->tm_year + 1900;   // get year since 1900
 
-    snprintf(buff, MAX_BUF, "%02d/%02d/%d %02d:%02d:%02d", day, month, year, hours, minutes, seconds);
-
+    snprintf(buff, MAX_BUF, "%02d/%02d/%d-%02d:%02d:%02d", day, month, year, hours, minutes, seconds);
 }
