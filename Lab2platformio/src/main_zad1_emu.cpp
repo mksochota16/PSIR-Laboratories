37c1754ae964ad97b77b71034cefa79a104bb625
shaggy17goo <michalwawrzynczak00@gmail.com>
2022-04-23 15:09:59 -0400

    zad1

--- Lab2platformio/src/main_zad1_emu.cpp
+++ Lab2platformio/src/main_zad1_emu.cpp
@@ -1,11 +1,11 @@
-/*
 #include <Arduino.h>
 #include <ZsutEthernet.h>
 #include <ZsutEthernetUdp.h>
 #include <ZsutFeatures.h>
 
 int localPort = 8888;
-int serverPort = 12397;
+int serverPort = 12361;
+int period = 7610;
 byte mac[] = {
         0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
@@ -21,11 +21,15 @@ void setup() {
 
 void loop() {
     Udp.beginPacket(ip, serverPort);
-    int sensorValue = (int) ZsutAnalog0Read();
-    String str = "A0: " + String(sensorValue) + "\n";
+    int sensorValue = (int) ZsutAnalog5Read();
+    String str = String(ZsutMillis()) + " Z5: " + String(sensorValue) + "\n";
+    Serial.print(F("SEND: "));
+    Serial.println(str);
     Udp.write(str.c_str(), str.length());
     Udp.endPacket();
-    delay(1000);
-}
 
-*/
+    unsigned time = ZsutMillis();
+    while (ZsutMillis() < time + period) {
+        //wait approx. [period] ms
+    }
+}
